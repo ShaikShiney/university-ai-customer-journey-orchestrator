fc91597dd8e061c3465f8b31ed2c8cb2a6b3e8a4
shiney <sshiney968@gmail.com>
2026-09-12 18:21:41 +0530

    Connect backend to Aiven MySQL

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,4 @@
 const mysql = require("mysql2/promise");
-
 require("dotenv").config();
 
 const pool = mysql.createPool({
@@ -8,9 +7,11 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
   ssl: {
     rejectUnauthorized: false
   },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
